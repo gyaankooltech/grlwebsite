034fbcf08f1dde4fb9c18a975aeb7838b895a9ba
gyaankooltech <gyaankool.database@gmail.com>
2025-11-26 16:51:07 +0530

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -25,9 +25,8 @@ export const Footer = () => {
                   GyaanKool Research Labs Private <br /> 
                   <span className="font-bold">CIN:</span> U62090KA2025PTC204517 <br />
                   <span className="font-bold">Grievance mail:</span> grievanceatgrl@gmail.com  <br />
-                  {/* Phone number:+91 97417 18160 <br /> */}
-                  { /* <span className="font-bold"> */ } Registered Address:</span> Om Chambers Binnamangala, Stage 1, Indiranagar, <br />
-                  Bengaluru, Karnataka 560038 <br />
+                  { /* <span className="font-bold"> */ } Registered Address:</span> Om Chambers Binnamangala, Stage 1, Indiranagar,
+                  Bengaluru, Karnataka 560038 
                   
                 </p>
               </div>
